2cd4e1ae5ad7590aa2b0402657c03b4f79e7107a
Vikram Nilaji <vik.nilaji@gmail.com>
2026-03-04 00:02:29 +0530

    modified Schema

--- auth-portal/backend/models/User.js
+++ auth-portal/backend/models/User.js
@@ -1,13 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const UserSchema = new mongoose.Schema({
-//   username: { type: String, required: true, unique: true },
-//   email: { type: String, required: true, unique: true },
-//   password: { type: String, required: true },
-// });
-
-// module.exports = mongoose.model('User', UserSchema);
-
 const mongoose =require("mongoose");
 const UserSchema=new mongoose.Schema({
     username:{type:String,required:true,unique:true},
