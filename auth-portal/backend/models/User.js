c1b5c417917e3267bc58615365904b5daffdaddb
Vikram <vik.nilaji@gmail.com>
2026-03-10 12:12:23 +0530

    changes in User.js

--- auth-portal/backend/models/User.js
+++ auth-portal/backend/models/User.js
@@ -7,6 +7,15 @@
 
 // module.exports=mongoose.model('User',UserSchema)
 
+// import mongoose from "mongoose";
+
+// const userSchema = new mongoose.Schema({
+//   name: String,
+//   email: String,
+//   password: String
+// });
+
+// export default mongoose.model("User", userSchema);
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
