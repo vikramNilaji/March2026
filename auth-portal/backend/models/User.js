be71c3ee1453926d7a1df24130e38fdd135746a6
Vikram <vik.nilaji@gmail.com>
2026-03-10 12:14:36 +0530

    deleted something

--- auth-portal/backend/models/User.js
+++ auth-portal/backend/models/User.js
@@ -7,15 +7,7 @@
 
 // module.exports=mongoose.model('User',UserSchema)
 
-// import mongoose from "mongoose";
 
-// const userSchema = new mongoose.Schema({
-//   name: String,
-//   email: String,
-//   password: String
-// });
-
-// export default mongoose.model("User", userSchema);
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
