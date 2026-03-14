804705fdf8865881d3f788dc9844a01d86e112bf
Vikram Nilaji <vik.nilaji@gmail.com>
2026-03-14 21:56:51 +0530

    add

--- auth-portal/frontend/signup/src/components/Signup.jsx
+++ auth-portal/frontend/signup/src/components/Signup.jsx
@@ -1,4 +1,82 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+
+// const Signup = () => {
+//   const [formData, setFormData] = useState({
+//     name: "",
+//     email: "",
+//     password: "",
+//   });
+//   const formdataHandle = (e) => {
+//     setFormData({ ...formData, [e.target.name]: e.target.value });
+//   };
+//   const navigate = useNavigate();
+
+//   const handleSubmit = async (e) =>  {
+//     e.preventDefault();
+//     try {
+//       const response = await fetch("http://localhost:3000/signup", {
+//         method: "POST",
+//         headers: { "Content-Type": "application/json" },
+//         body: JSON.stringify(formData),
+//       });
+
+//       const data = await response.json();
+
+//       if (response.ok) {
+//         alert("Signup successful! Please login.");
+//         navigate("/login");
+//       } else {
+//         alert(data.error || "Signup failed");
+//       }
+//     } catch (err) {
+//       console.error("Error:", err);
+//     }
+//   };
+
+//   return (
+//     <div style={{ maxWidth: "400px", margin: "50px auto" }}>
+//       <h2>Create Account</h2>
+//       {/* <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+//         <input type="text" placeholder="Full Name" onChange={(e) => setFormData({...formData, name: e.target.value})} required />
+//         <input type="email" placeholder="Email" onChange={(e) => setFormData({...formData, email: e.target.value})} required />
+//         <input type="password" placeholder="Password" onChange={(e) => setFormData({...formData, password: e.target.value})} required />
+//         <button type="submit">Sign Up</button>
+//       </form> */}
+
+//       <form
+//         onSubmit={handleSubmit}
+//         style={{ display: "flex", flexDirection: "column", gap: "10px" }}
+//       >
+//         <input
+//           type="text"
+//           placeholder="Full Name"
+//           name="name"
+//           onChange={formdataHandle}
+//           required
+//         />
+//         <input
+//           type="email"
+//           placeholder="Email"
+//           name="email"
+//           onChange={formdataHandle}
+//           required
+//         />
+//         <input
+//           type="password"
+//           placeholder="Password"
+//           name="password"
+//           onChange={formdataHandle }
+//           required
+//         />
+//         <button type="submit">Sign Up</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
@@ -7,71 +85,38 @@ const Signup = () => {
     email: "",
     password: "",
   });
-  const formdataHandle = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+  const InputHandle = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
   };
-  const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetch("http://localhost:3000/signup", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
-      });
+  const navigate=useNavigate();
 
-      const data = await response.json();
+  const HandleSubmit(){
 
-      if (response.ok) {
-        alert("Signup successful! Please login.");
-        navigate("/login");
-      } else {
-        alert(data.error || "Signup failed");
-      }
-    } catch (err) {
-      console.error("Error:", err);
-    }
-  };
+  }
 
   return (
-    <div style={{ maxWidth: "400px", margin: "50px auto" }}>
-      <h2>Create Account</h2>
-      {/* <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-        <input type="text" placeholder="Full Name" onChange={(e) => setFormData({...formData, name: e.target.value})} required />
-        <input type="email" placeholder="Email" onChange={(e) => setFormData({...formData, email: e.target.value})} required />
-        <input type="password" placeholder="Password" onChange={(e) => setFormData({...formData, password: e.target.value})} required />
-        <button type="submit">Sign Up</button>
-      </form> */}
-
-      <form
-        onSubmit={handleSubmit}
-        style={{ display: "flex", flexDirection: "column", gap: "10px" }}
-      >
-        <input
-          type="text"
-          placeholder="Full Name"
-          name="name"
-          onChange={formdataHandle}
-          required
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          name="email"
-          onChange={formdataHandle}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          name="password"
-          onChange={formdataHandle }
-          required
-        />
-        <button type="submit">Sign Up</button>
-      </form>
-    </div>
+    <form onSubmit={HandleSubmit}>
+      <input
+        type="text"
+        placeholder="name"
+        name="name"
+        onChange={InputHandle}
+      />
+      <input
+        type="text"
+        placeholder="email"
+        name="email"
+        onChange={InputHandle}
+      />
+      <input
+        type="text"
+        placeholder="password"
+        name="password"
+        onChange={InputHandle}
+      />
+      <button type="submit">Submit</button>
+    </form>
   );
 };
 
